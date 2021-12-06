3ba034c3b9aae8376b8531af5b9118d333587910
hacho2011 <hacho_2011@naver.com>
2021-12-06 23:01:42 +0900

    habit tracker compltete

--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -1,40 +1,52 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from "react";
 
-class Habit extends Component {
-    state = {
-        count:0,
-    }
-    handleIncreament = () => {
-        this.setState({count:this.state.count +1})
-    }
+class Habit extends PureComponent {
+  componentDidMount() {
+    console.log("생성");
+  }
 
-    handleDecreament = () => {
-        const count = this.state.count -1;
-        this.setState({count: count < 0? 0: count})
-    }
-    
-    
-    render() {
-        const {name, count} = this.props.habit;
-        return (
-            <li className="habit">
-                <span className="habit-name">{name}</span>
-                <span className="habit-count">{count}</span>
-                <button className="habit-button habit-increase"
-                onClick={this.handleIncreament}>
-                    <i className="fas fa-plus-square"></i>
-                </button>
-                <button className="habit-button habit-decrease"
-                onClick={this.handleDecreament}>
-                    <i className="fas fa-minus-square"></i>
-                </button>
-                <button className="habit-button habit-delete">
-                    <i class="fas fa-trash"></i>
-                </button>
-            </li>
+  componentWillMount() {
+    console.log("삭제");
+  }
+  handleIncrement = () => {
+    this.props.onIncrement(this.props.habit);
+  };
 
-        );
-    }
+  handleDecrement = () => {
+    this.props.onDecrement(this.props.habit);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  };
+
+  render() {
+    const { name, count } = this.props.habit;
+    return (
+      <li className="habit">
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
+          <i className="fas fa-minus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
+          <i className="fas fa-trash"></i>
+        </button>
+      </li>
+    );
+  }
 }
 
 export default Habit;
--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import Habit from './habit';
+import HabitAddForm from './habitAddForm';
 
 class Habits extends Component {
-    state = {
-        habits : [
-            {id:1, name: 'Reading', count:0},
-            {id:2, name: 'Running', count:0},
-            {id:3, name: 'Coding', count:0},
-        ]
-    };
-    render() {
-        return (
-            <ul>
-                {
-                    this.state.habits.map(habit => (
-                        <Habit key={habit.id} habit={habit}/>
-                    ))
-                }
-            </ul>
-        );
-    }
+  render() {
+    return (
+      <div className="habits">
+        <HabitAddForm onAdd={this.props.onAdd} />
+        <ul>
+          {this.props.habits.map(habit => (
+            <Habit
+              key={habit.id}
+              habit={habit}
+              onIncrement={this.props.onIncrement}
+              onDecrement={this.props.onDecrement}
+              onDelete={this.props.onDelete}
+            />
+          ))}
+        </ul>
+        <button className="habits-reset" onClick={this.props.onReset}>
+          Reset All
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Habits;
